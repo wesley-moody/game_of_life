389b2b53688b2c6f6e5dd55a034ffc7e0ef4961d
wesley.moody <wgm3190@gmail.com>
2020-07-27 12:26:45 -0500

    started project and put placeholder css in

--- project/src/index.js
+++ project/src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class Grid extends React.Component {
+  render() {
+    return <div>Grid</div>;
+  }
+}
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      generation: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>The Game of Life</h1>
+        <Grid />
+        <h2>Generations: {this.state.generation}</h2>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
