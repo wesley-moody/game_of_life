67f6ef274e1a9cd113d240dab8851c943ac51440
wesley.moody <wgm3190@gmail.com>
2020-07-27 13:54:36 -0500

    grid rendering

--- project/src/index.js
+++ project/src/index.js
@@ -2,16 +2,65 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+class Box extends React.Component {
+  selectBox = () => {
+    this.props.selectBox(this.props.row, this.props.col);
+  };
+  render() {
+    return (
+      <div
+        className={this.props.boxClass}
+        id={this.props.id}
+        onClick={this.selectBox}
+      />
+    );
+  }
+}
+
 class Grid extends React.Component {
   render() {
-    return <div>Grid</div>;
+    const width = this.props.cols * 16 + 1;
+    var rowsArr = [];
+
+    var boxClass = "";
+    for (var i = 0; i < this.props.rows; i++) {
+      for (var j = 0; j < this.props.rows; j++) {
+        let boxId = i + "_" + j;
+
+        boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+        rowsArr.push(
+          <Box
+            boxClass={boxClass}
+            key={boxId}
+            boxId={boxId}
+            row={i}
+            col={j}
+            selectBox={this.props.selectBox}
+          />
+        );
+      }
+    }
+
+    return (
+      <div className='grid' style={{ width: width }}>
+        {rowsArr}
+      </div>
+    );
   }
 }
 class Main extends React.Component {
   constructor() {
     super();
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
+
     this.state = {
+      //  in gen 0 there's a 50 x 30 grid with all cells turned off to start
       generation: 0,
+      gridFull: Array(this.rows)
+        .fill()
+        .map(() => Array(this.cols).fill(false)),
     };
   }
 
@@ -19,7 +68,12 @@ class Main extends React.Component {
     return (
       <div>
         <h1>The Game of Life</h1>
-        <Grid />
+        <Grid
+          gridFull={this.state.gridFull}
+          rows={this.rows}
+          cols={this.cols}
+          selectBox={this.selectBox}
+        />
         <h2>Generations: {this.state.generation}</h2>
       </div>
     );
